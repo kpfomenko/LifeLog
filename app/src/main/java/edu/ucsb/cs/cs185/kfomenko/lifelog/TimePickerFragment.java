f01ac44d9ab5e339a4ae8b4a37e1818203259363
kpfomenko <kpfomenko@gmail.com>
2015-06-06 23:23:39 -0700

    Made Create Entry into an Activity--> Time picker is now functioning

--- app/src/main/java/edu/ucsb/cs/cs185/kfomenko/lifelog/TimePickerFragment.java
+++ app/src/main/java/edu/ucsb/cs/cs185/kfomenko/lifelog/TimePickerFragment.java
@@ -1,8 +1,10 @@
 package edu.ucsb.cs.cs185.kfomenko.lifelog;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.widget.Button;
@@ -15,6 +17,22 @@ import java.util.Calendar;
  * Created by Katya on 6/6/2015.
  */
 public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+//    OnChangeListener mCallback;
+    int calledBtnVal;
+//    public interface OnChangeListener{
+//        public void onChangeOccured(int hourOfDay, int minute, int calledBtnVal);
+//    }
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        try {
+//            mCallback = (OnChangeListener) activity;
+//        } catch (ClassCastException e) {
+//            throw new ClassCastException(activity.toString()
+//                    + " must implement OnHeadlineSelectedListener");
+//        }
+//    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current time as the default values for the picker
@@ -28,7 +46,61 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
     }
 
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        String am_pm = "";
 
+        Calendar datetime = Calendar.getInstance();
+        datetime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        datetime.set(Calendar.MINUTE, minute);
+
+        if (datetime.get(Calendar.AM_PM) == Calendar.AM)
+            am_pm = "AM";
+        else if (datetime.get(Calendar.AM_PM) == Calendar.PM)
+            am_pm = "PM";
+
+        String strHrsToShow = (datetime.get(Calendar.HOUR) == 0) ?"12":datetime.get(Calendar.HOUR)+"";
+
+        String output = String.format("%s:%02d %s", strHrsToShow, minute, am_pm);
+
+        if(calledBtnVal == 0){
+            Button startBtn = (Button) getActivity().findViewById(R.id.entry_start_time_btn);
+            startBtn.setText(output);
+        }
+        if(calledBtnVal == 1){
+            Button endBtn = (Button) getActivity().findViewById(R.id.entry_end_time_btn);
+            endBtn.setText(output);
+        }
     }
 
+    public void setBtnVal(int val){
+        calledBtnVal = val;
+    }
 }
+
+//public class TimePickerFragment extends TimePickerDialog{
+//
+//    public TimePickerFragment(Context arg0, OnTimeSetListener callBack, int hourOfDay, int minute, boolean is24HourView) {
+//        super(arg0, callBack, hourOfDay, minute, is24HourView);
+//        // TODO Auto-generated constructor stub
+//    }
+//
+//    @Override
+//    public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+//        // TODO Auto-generated method stub
+//        //super.onTimeChanged(arg0, arg1, arg2);
+//        if (mIgnoreEvent)
+//            return;
+//        if (minute%TIME_PICKER_INTERVAL!=0){
+//            int minuteFloor=minute-(minute%TIME_PICKER_INTERVAL);
+//            minute=minuteFloor + (minute==minuteFloor+1 ? TIME_PICKER_INTERVAL : 0);
+//            if (minute==60)
+//                minute=0;
+//            mIgnoreEvent=true;
+//            view.setCurrentMinute(minute);
+//            mIgnoreEvent=false;
+//        }
+//    }
+//
+//    private final int TIME_PICKER_INTERVAL=15;
+//    private boolean mIgnoreEvent=false;
+//
+//}
